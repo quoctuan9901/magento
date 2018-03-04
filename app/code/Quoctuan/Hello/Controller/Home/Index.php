70f22a3f078e17aefb6fa69f9591d396d7838b6c
quoctuan9901 <contact.quoctuan@gmail.com>
2018-03-04 06:58:06 +0000

    layout,block,template

--- app/code/Quoctuan/Hello/Controller/Home/Index.php
+++ app/code/Quoctuan/Hello/Controller/Home/Index.php
@@ -9,10 +9,18 @@
 namespace Quoctuan\Hello\Controller\Home;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action {
+    protected $_pageFactory;
+
+    public function __construct(Context $context) {
+        parent::__construct($context);
+        $this->_pageFactory = $this->_objectManager->get(PageFactory::class);
+    }
+
     public function execute() {
-        echo "Module Hello Frontend - Home Controller - Index Action";
-        exit();
+        return $this->_pageFactory->create();
     }
 }
